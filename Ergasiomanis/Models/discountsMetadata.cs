1ccecf8ee9ff191df917e6df4b5f35fe523ad8ce
GeorgePapei <r.george@hotmail.gr>
2021-01-24 22:53:07 +0200

    teleiwsa discounts metadata

--- Ergasiomanis/Models/discountsMetadata.cs
+++ Ergasiomanis/Models/discountsMetadata.cs
@@ -10,7 +10,24 @@ namespace Ergasiomanis.Models
     {
         [Display(Name = "Discount Type")]
         [Required]
-        [StringLength()]
+        [StringLength(40, ErrorMessage = "Discount Type should not exceed 40 characters!")]
         public string discounttype;
+
+        [Display(Name = "Store id")]
+        [Required]
+        [StringLength(4, MinimumLength = 4, ErrorMessage = "Store id should be 4 characters!")]
+        public string stor_id;
+
+        [Display(Name = "Low quantity")]
+        [Required]
+        public string lowqty;
+
+        [Display(Name = "High quantity")]
+        [Required]
+        public string highqty;
+
+        [Display(Name = "Discount")]
+        [Required]
+        public decimal discount;
     }
 }
